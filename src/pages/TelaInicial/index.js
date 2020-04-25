b547434204e22602fc004d08924095104d376889
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2020-04-25 19:43:06 -0300

    Adicionei o texto

--- src/pages/TelaInicial/index.js
+++ src/pages/TelaInicial/index.js
@@ -6,7 +6,15 @@ export default class TelaInicial extends React.Component {
         return(
         <div>
             <div className="header"><h1>Sabaconverter</h1></div>
-            <div className="mainText"></div>
+            <div className="mainText">
+                <h2 className="title">Como funciona?</h2>
+                <p className="text">
+                Seja bem vindo ao Sabaconverter! Como gamers nos sabemos da tristeza 
+                que é quando vc encontra sua sensibilidade perfeita para um jogo mas não é a mesma para outro. 
+                Por isso, criamos uma ferramenta que possa proporcionar uma experiencia mais igualitaria entre seus jogos
+                de maneira que voce se sinta confortavel com a sua sensibilidade em todos eles.
+                </p>
+            </div>
         </div>
         )
     }
