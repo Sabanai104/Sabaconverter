bba61ba3e3e7455e6c836c422c589d57db8d6e71
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2020-05-02 18:07:13 -0300

    Updates

---
+++ precache-manifest.eba8d384b70ad8a3092839d7b4fd0e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8dd37a899bcdda839573135479f767",
+    "url": "/Sabaconverter/index.html"
+  },
+  {
+    "revision": "ce914384a5dd857d0115",
+    "url": "/Sabaconverter/static/css/main.40c836a9.chunk.css"
+  },
+  {
+    "revision": "67870a3719843cb3568d",
+    "url": "/Sabaconverter/static/js/2.150c81f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sabaconverter/static/js/2.150c81f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce914384a5dd857d0115",
+    "url": "/Sabaconverter/static/js/main.eee6873c.chunk.js"
+  },
+  {
+    "revision": "bcd92d3cd837a6c614d3",
+    "url": "/Sabaconverter/static/js/runtime-main.3951c5bd.js"
+  },
+  {
+    "revision": "4a747119eb0bbf7789e11fb43486b401",
+    "url": "/Sabaconverter/static/media/mouseImg.4a747119.png"
+  }
+]);
